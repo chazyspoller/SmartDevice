b55e2ddaee451ddf4e727a03883504e09c97ffb6
chazyspoller <chazyspoller@gmail.com>
2022-05-17 17:31:52 +0300

    Create footer and add webp

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import './modules/modals/footer-links';
 
 // ---------------------------------
 
@@ -16,22 +17,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
 
 // для адаптивного JS используейтся matchMedia и addListener
 // const breakpoint = window.matchMedia(`(min-width:1024px)`);
@@ -42,5 +30,3 @@ window.addEventListener('DOMContentLoaded', () => {
 // };
 // breakpoint.addListener(breakpointChecker);
 // breakpointChecker();
-
-// используйте .closest(el)
