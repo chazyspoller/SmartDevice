af6463db1094d6eaf3a8e98012560b8b7580887f
chazyspoller <chazyspoller@gmail.com>
2022-05-18 11:16:56 +0300

    Create advantages section

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import './modules/modals/footer-links';
+import './modules/modals/advantages';
 
 // ---------------------------------
 
